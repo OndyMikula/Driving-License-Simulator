aa2017ce0561988b9199a0ea0967a65f4f23655a
Ondy <zoufalyondrej@gmail.com>
2025-05-28 23:04:42 +0200

    Added map & character pack

---
+++ Assets/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // dat mesto
+    // chodce
+    // krizovatky kde najednou prijedou 3 auta a ty musis vybrat v jakym poradi pojedou a system overi jestlis to udelal spravne nebo spatne
+    // features: srazis chodce - bonusovy body
+    //           nabouras do auta - bonusovy body
+    //           nabouras do baraku - bonusovy body body
+    //           nahodny npc bude zdenda a bude rikat KAPR S NIVOU
+    //           schovas rl arenu kde budes moct realne hrat rocket league s boostem a skokem a micem a vsim
+    //           na klavesu budes mit italskej brainrot nahodnej
+    //           na klavesu budes mit nahodny hlasky zdendy
+    //           na klavesu se objevi ptacek co bude nad tebou poletovat a bude rvat "Dobrý DEN"
+    //           pri prujezdu okolo chodcu budes moct (treba na klavesu) rict Dobrý DEN a oni odpovi Ty čůráku deblní vole 
+    //           budes sbirat zdenda coiny
+    //           bude schovanej giga obrazek zdendy co kdyz ho najdes tak te bude honit pres celou mapu a furt se ti bude smat a rikat KAPR S NIVOU
+    //           bude tam skrytej easter egg s kaprem a zdendou
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
